0f4f0e3939021020e30b0362eaaf799bc10a6325
Benahin Kur <benahink@gmail.com>
2019-05-13 16:15:33 -0600

    working delete button

--- exercises/moms-shopping-list-start/index.js
+++ exercises/moms-shopping-list-start/index.js
@@ -1,29 +1,34 @@
 document.getElementById('add-todo').addEventListener('submit', function(e){
     e.preventDefault()
-    let addedItem = document.getElementById('title').value
-    let list = document.getElementById('list')
-    let li = document.createElement('li')
-    let newDiv = document.createElement('div')
-    newDiv.innerText = `${addedItem}`
-    newDiv.setAttribute('class', 'item')
-    let xButton = document.createElement('button')
-    xButton.setAttribute('id', 'delete')
-    xButton.innerText = "X"
-    let editButton = document.createElement('button')
-    editButton.setAttribute('id', 'change')
-    editButton.innerText = "edit"
-    list.appendChild(li)
-    li.appendChild(newDiv)
-    li.appendChild(editButton)
-    li.appendChild(xButton)
+    let addedItem = document.getElementById('title').value;
+    let list = document.getElementById('list');
+    let li = document.createElement('li');
+    let newDiv = document.createElement('div');
+    let xButton = document.createElement('button');
+    let editButton = document.createElement('button');
+    
+    newDiv.innerText = `${addedItem}`;
+    xButton.innerText = "X";
+    editButton.innerText = "edit";
+    
+    newDiv.setAttribute('class', 'item');
+    xButton.setAttribute('id', 'delete');
+    editButton.setAttribute('id', 'change');
+    xButton.addEventListener('click', function(e) {
+        e.preventDefault
+        this.parentNode.remove();
+    })
+    // editButton.addEventListener.length('submit', function() {
+    //     this.parentNode.
+    // })
+    
+    list.appendChild(li);
+    li.appendChild(newDiv);
+    li.appendChild(editButton);
+    li.appendChild(xButton);
 })
 
-document.getElementById('list').addEventListener('click', function (e) {
-    e.preventDefault()
-    let deleteItem = document.getElementById('button')
-    console.log(deleteItem)
-    deleteItem.remove()
-})
+
 
 
 
