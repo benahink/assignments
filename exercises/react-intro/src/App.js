fafedef8c5db8b7b17254606e95aec34faa37a18
Benahin Kur <benahink@gmail.com>
2019-05-13 17:03:36 -0600

    the bare bones of react

---
+++ exercises/react-intro/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './Navbar'
+import Main from './Main'
+import Footer from './Footer'
+import './styles.css'
+
+const App = () => {
+    return (
+        <>
+            <NavBar />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
