b22c57a0d8044f844d2dede749714ae7fe70b96b
Benahin Kur <benahink@gmail.com>
2019-05-13 11:43:54 -0600

    made comments more detailed

--- exercises/es6-slope-style/app.js
+++ exercises/es6-slope-style/app.js
@@ -1,11 +1,13 @@
 // ****Green dot ski slope****
 // Q1
+// Use the Rest Operator to help this function return an array of animals, no matter how many animals are passed to it:
 const collectAnimals = (...arr) => {
     return arr;
 }
 console.log(collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"));
 
 // Q2
+// Write a function that returns a food object with the array names as properties using Object Literals:
 const combineFruits = (fruits, sweets, vegetables) => {
     const obj = {
         fruits,
@@ -19,6 +21,7 @@ console.log(combineFruits(["apple", "pear"],
 ["carrit"]));
 
 // Q3
+// Use destructuring to fill in the blanks and use the property names as variables:
 const vacations = {
     location: 'Burly Idaho', 
     duration: "2 weeks"
@@ -34,6 +37,7 @@ const parseSentence = () => {
 console.log(parseSentence())
 
 // Q4
+// Use destructuring to make this code ES6:
 const returnFirst = (items) => {
     let [firstName] = items;
     return firstName;
@@ -42,6 +46,8 @@ const returnFirst = (items) => {
 console.log(returnFirst(["carrot", "tomato", "tomato soup"]));
 
 // Q5
+// Write destructuring code to assign variables that will help us return the expected string. 
+// Also, change the string to be using Template literals:
 const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
 
 const returnFavorites = (arr) => {
@@ -52,6 +58,9 @@ console.log(returnFavorites(favoriteActivities));
 
 // ****Blue square ski slope****
 // Q1
+// Use the Rest and Spread Operator to help take any number of arrays, 
+// and return one array. You will need to change how the arrays are passed in. 
+// You may write it assuming you will always recieve three arrays if you would like to.
 const combineArrays = (arr1, arr2, arr3) => {
     const combine = [...arr1, ...arr2, ...arr3]
     return combine
@@ -60,3 +69,9 @@ const realAnimals = ["dog", "cat", "mouse"];
 const magicalAnimals = ["jackolope"];
 const mysteriousAnimals = ["platypus"];
 console.log(combineArrays(realAnimals, magicalAnimals, mysteriousAnimals));
+
+
+// ****Black diamond ski slope****
+// Q1
+// Try to make the following function more ES6xy:
+
