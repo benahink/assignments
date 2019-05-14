78e043f4cb2b95bcec1b6df73bf2e2d8e2ec098c
Benahin Kur <benahink@gmail.com>
2019-05-14 13:45:30 -0600

    created my containers

---
+++ exercises/business-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Main from './Main/Main'
+import NavBar from './NavBar/NavBar'
+import Footer from './Footer/Footer'
+
+const App = () => {
+    return (
+        <>
+        <Main />
+        <NavBar />
+        <Footer />
+        </>
+    )
+}
+
+export default App
---
+++ exercises/business-react/src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="navbar-container">
+            <div id="home">Home</div>
+            <div id="info">Info</div>
+            <div id="products">Products</div>
+        </div>
+    )
+}
+
+export default NavBar
