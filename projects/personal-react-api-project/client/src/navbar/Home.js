8f9f45d55669d5e638fb66d9fda0e56e24c89bcf
Benahin Kur <benahink@gmail.com>
2019-06-30 15:14:47 -0600

    commit

--- projects/personal-react-api-project/client/src/navbar/Home.js
+++ projects/personal-react-api-project/client/src/navbar/Home.js
@@ -3,11 +3,23 @@ import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import Logout from "../logout/Logout.js";
 import './Home.css';
 import './Navbar.css';
+// import avatar from "../images/avatar.png";
 
 const Home = (props) => {
     return (
        <div className="App">
                 <div className="App__Form">
+                    <span class="aui-avatar aui-avatar-xxlarge">
+                        <span class="aui-avatar-inner">
+                            <img src="../images/avatar.png" alt="Person McRealface" />
+                        </span>
+                        <span class="custom-presence-indicator">
+                            <svg height="100%" version="1.1" viewBox="0 0 8 8" width="100%" xmlns="http://www.w3.org/2000/svg">
+                                <description>Online</description>
+                                <circle cx="4" cy="4" r="4"></circle>
+                            </svg>
+                        </span>
+                    </span>
                     <div className="PageSwitcher">
                         <NavLink to="/signin" 
                                 activeClassName="PageSwitcher__Item--Active"
