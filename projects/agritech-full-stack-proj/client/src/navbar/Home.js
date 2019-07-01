b1a4b6be0962d4d99e95f3da77d6f2a768eac7a4
Benahin Kur <benahink@gmail.com>
2019-07-01 14:37:06 -0600

    commit

--- projects/agritech-full-stack-proj/client/src/navbar/Home.js
+++ projects/agritech-full-stack-proj/client/src/navbar/Home.js
@@ -3,6 +3,7 @@ import { Route, NavLink } from 'react-router-dom';
 import Logout from "../logout/Logout.js";
 import Messages from './Messages.js';
 import DisplayPanels from '../accordion/DisplayPanels.js';
+import { Header, Image } from 'semantic-ui-react';
 import './Home.css';
 import './Navbar.css';
 
@@ -10,9 +11,11 @@ const Home = (props) => {
     return (
         <>
             <div className="App">
-                    <div className="App__Form">
-                        <div>
+                <div className="App__Form">
                             <div className="PageSwitcher">
+                                <NavLink to="/newsfeed" 
+                                        activeClassName="PageSwitcher__Item--Active"
+                                        className="PageSwitcher__Item">Messages</NavLink>
                                 <NavLink to="/newsfeed" 
                                         activeClassName="PageSwitcher__Item--Active"
                                         className="PageSwitcher__Item">Messages</NavLink>
@@ -20,17 +23,20 @@ const Home = (props) => {
                                         activeClassName="PageSwitcher__Item--Active"
                                         className="PageSwitcher__Item">Logout</NavLink>
                             </div>
-                            <Messages />
-                        </div>
-                        {/* <Route exact path="/" component={SignUp}></Route> */}
-                        <Route path="/" component={Logout}></Route> 
-                    </div>  
-                    <div className="App__Aside">
-                        <DisplayPanels />
-                    </div>
+                    <Messages />
+                    {/* <Route exact path="/" component={SignUp}></Route> */}
+                    <Route path="/" component={Logout}></Route> 
+                </div>  
+                <div className="App__Aside">
+                        <Header as='h2' className="avatar">
+                            Your Name
+                            <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' /> 
+                        </Header>
+                    <DisplayPanels />
                 </div>
-            </>
-    );
+            </div>
+        </>
+);
 }
  
 export default Home;
--- projects/agritech-full-stack-proj/client/src/navbar/Navbar.js
+++ projects/agritech-full-stack-proj/client/src/navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withProvider } from '../GlobalProvider.js';
-import { Header, Image } from 'semantic-ui-react';
 import './Navbar.css';
 
 const Navbar = (props) => {
@@ -10,10 +9,6 @@ const Navbar = (props) => {
             <div className="bar"></div>
             <div className="header">
                 <h1 className="app-name">Farming Medium</h1>
-                <Header as='h2' className="avatar">
-                    Your Name
-                    <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' /> 
-                </Header>
             </div>
             <div className="navbar">
                 <Link to="/myprofile">Account</Link>
