040dbbfa90190740a3d9aa77c0a369b1ff960568
benahink <benahink@gmail.com>
2019-08-19 17:31:02 -0600

    commit color scheme

--- projects/portfolio/src/work/WorkCard.js
+++ projects/portfolio/src/work/WorkCard.js
@@ -11,21 +11,21 @@ class WorkCard extends Component {
                         <h2 className="front-end_title">Front-end</h2>
                         {/* <div className="solid-line_skills"></div> */}
                         <ul className="front-end_icons">
-                            <i className="fab fa-html5 fa-5x"></i>
-                            <i className="fab fa-css3-alt fa-5x"></i>
-                            <i className="fab fa-js fa-5x"></i>
-                            <i className="fab fa-sass fa-5x"></i>
-                            <i className="fab fa-npm fa-5x"></i>
-                            <i class="fab fa-react fa-5x"></i>
-                            {/* <i class="fab fa-node fa-5x"></i> */}
-                            <i class="fab fa-github fa-5x"></i>
+                            <i className="fab fa-html5 fa-3x"></i>
+                            <i className="fab fa-css3-alt fa-3x"></i>
+                            <i className="fab fa-js fa-3x"></i>
+                            <i className="fab fa-sass fa-3x"></i>
+                            <i className="fab fa-npm fa-3x"></i>
+                            <i class="fab fa-react fa-3x"></i>
+                            {/* <i class="fab fa-node fa-3x"></i> */}
+                            <i class="fab fa-github fa-3x"></i>
                         </ul>
                     </div>
                     <div className="mySkills section-two">
                         <h2 className="front-end_title">Back End</h2>
                         <ul className="back-end_icons">
-                            <i className="fab fa-java fa-5x"></i>
-                            <i class="fab fa-node-js fa-5x"></i>
+                            <i className="fab fa-java fa-3x"></i>
+                            <i class="fab fa-node-js fa-3x"></i>
                         </ul>
                     </div>
                     <div className="mySkills section-three">
